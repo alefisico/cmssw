9e12da2d9d50167fc37b3ab45bd706b270eef528
Alejandro <gomez@rutgers.physics.edu>
2015-01-22 11:27:19 -0600

    stable version

--- RecoJets/JetProducers/python/jetToolbox_cff.py
+++ RecoJets/JetProducers/python/jetToolbox_cff.py
@@ -3,8 +3,13 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff import *
 from RecoJets.Configuration.RecoPFJets_cff import *
 from RecoJets.Configuration.RecoGenJets_cff import * 
+from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
+from PhysicsTools.PatAlgos.tools.jetTools import switchJetCollection
 
-def jetToolbox( proc, jetType, jetSequence, outputFile, addSubstructure=True, addGroomers=True, addNsub=True, addNsubUpTo5=False, addQJets=True, addSubjets=False, minPt=100. ):
+#process.load('PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff')
+
+
+def jetToolbox( proc, jetType, jetSequence, outputFile, addSubstructure=True, addGroomers=True, addNsub=True, addNsubUpTo5=False, addQJets=True, addSubjets=False, minPt=100., miniAOD=False ):
 	
 	###############################################################################
 	#######  Just defining simple variables
@@ -26,6 +31,7 @@ def jetToolbox( proc, jetType, jetSequence, outputFile, addSubstructure=True, ad
 	### Trick for uppercase/lowercase algo name
 	jetALGO = jetAlgo.upper()+size
 	jetalgo = jetAlgo.lower()+size
+	if( int(size) > 10 ): size = '10' 	### For JEC for jets larger than 1 
 
 
 	#################################################################################
@@ -35,7 +41,41 @@ def jetToolbox( proc, jetType, jetSequence, outputFile, addSubstructure=True, ad
 	elemToKeep = []
 	jetSeq = cms.Sequence()
 
-	### For MiniAOD, we need to load packedPFCandidates
+	#print proc
+	#if hasattr( cms.InputTag(''), 'ak4PFJetsCHS' ):
+	#if hasattr( proc, 'packedPFCandidates' ):
+
+	#try:
+	#### For AOD
+	'''
+	if not miniAOD:
+		print 'YES'
+		proc.load('RecoJets.Configuration.GenJetParticles_cff')
+		proc.load('RecoJets.Configuration.RecoPFJets_cff')
+		setattr( proc, jetalgo+'GenJets', ak4GenJets.clone( src = 'genParticlesForJetsNoNu', rParam = jetSize, jetAlgorithm = algorithm ) ) 
+		jetSeq += getattr(proc, jetalgo+'GenJets' )
+
+		addJetCollection(
+				proc,
+				labelName = jetALGO+'PFCHS',
+				jetSource = cms.InputTag( jetalgo+'PFJetsCHS'),
+				algo = jetalgo,
+				rParam = jetSize,
+				jetCorrections = ( 'AK'+size+'PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
+				outputModules = ['outputFile'],
+				genJetCollection = cms.InputTag( jetalgo+'GenJets' ),
+				) 
+		elemToKeep += [ 'keep *_patJets'+jetALGO+'PFCHS_*_*' ]
+
+	else:
+	#setattr( proc, jetalgo+'PFJetsCHSPruned', ak8PFJetsCHSPruned.clone( src = 'patJets'+jetALGO+'PFCHS', rParam = jetSize, jetAlgorithm = algorithm ) )
+	#setattr( proc, jetalgo+'PFJetsCHSPrunedLinks', ak8PFJetsCHSPrunedLinks.clone( src = cms.InputTag( jetalgo+"PFJetsCHS"), 
+#		matched = cms.InputTag( jetalgo+'PFJetsCHSPruned'), distMax = cms.double( jetSize ) ) )
+	elemToKeep += [ 'keep *_'+jetalgo+'PFJetsCHSPrunedLinks_*_*'] 
+	jetSeq += getattr(proc, jetalgo+'PFJetsCHSPruned' )
+	jetSeq += getattr(proc, jetalgo+'PFJetsCHSPrunedLinks' )
+	getattr( proc, 'patJets'+jetALGO+'PFCHS').userData.userFloats.src += [ jetalgo+'PFJetsCHSPrunedLinks']
+	'''
 	setattr( proc, 'chs', cms.EDFilter('CandPtrSelector', src = cms.InputTag('packedPFCandidates'), cut = cms.string('fromPV')) )
 	jetSeq += getattr(proc, 'chs')
 
@@ -51,8 +91,6 @@ def jetToolbox( proc, jetType, jetSequence, outputFile, addSubstructure=True, ad
 	####  Creating PATjets
 	proc.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
 
-	if( int(size) > 10 ): size = '10' 	### For JEC for jets larger than 1 
-	from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
 	addJetCollection(
 			proc,
 			labelName = jetALGO+'PFCHS',
@@ -85,6 +123,7 @@ def jetToolbox( proc, jetType, jetSequence, outputFile, addSubstructure=True, ad
 	getattr( proc, 'impactParameterTagInfos' ).primaryVertex = cms.InputTag('unpackedTracksAndVertices')
 	getattr( proc, 'inclusiveSecondaryVertexFinderTagInfos' ).extSVCollection = cms.InputTag('unpackedTracksAndVertices','secondary','')
 	getattr( proc, 'combinedSecondaryVertex').trackMultiplicityMin = 1 #silly sv, uses un filtered tracks.. i.e. any pt
+
 
 	if ( addSubstructure ):
 
@@ -187,101 +226,3 @@ def jetToolbox( proc, jetType, jetSequence, outputFile, addSubstructure=True, ad
 							outputCommands = cms.untracked.vstring( elemToKeep ) ) )
 
 
-##### Still in progress #############
-#def jetToolboxFromAOD( proc, jetType, addGroomers = True, addNsub = True, addNsubUpTo5 = False, addQJets = True, minPt = 100. ):
-#	
-#	###############################################################################
-#	#######  Just defining simple variables
-#	###############################################################################
-#	supportedJetAlgos = { 'ak': 'AntiKt', 'ca' : 'CambridgeAachen', 'kt' : 'Kt' }
-#	jetAlgo = ''
-#	algorithm = ''
-#	size = ''
-#	for type, tmpAlgo in supportedJetAlgos.iteritems(): 
-#		if type in jetType.lower():
-#			jetAlgo = type
-#			algorithm = tmpAlgo
-#			size = jetType.replace( type, '' )
-#	if jetAlgo == '': print 'Unsupported jet algorithm. Please use something like: jetType = CA8'
-#
-#	jetSize = 0.
-#	if int(size) in range(0, 20): jetSize = int(size)/10.
-#	else: print 'jetSize has not a valid value. Insert a number between 1 and 20 after algorithm, like: AK8'
-#	### Trick for uppercase/lowercase algo name
-#	jetALGO = jetAlgo.upper()+size
-#	jetalgo = jetAlgo.lower()+size
-#
-#
-#	#################################################################################
-#	####### Toolbox start 
-#	#################################################################################
-#
-#	elemToKeep = []
-#
-#	from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
-#	addJetCollection( proc,
-#		labelName = jetALGO+'PFCHS',
-#		jetSource = cms.InputTag( jetalgo+'PFJetsCHS'),
-#		algo = jetalgo,
-#		rParam = jetSize,
-#		jetCorrections = ('AK'+size+'PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
-#		genJetCollection = cms.InputTag( jetalgo+'GenJets' ),
-#		)
-#	elemToKeep += [ 'keep *_'+jetalgo+'PFJetsCHS_*_*', 'keep *_patJets'+jetALGO+'PFCHS_*_*' ]
-#
-#	#### For big jets
-#	if ( jetSize > 0.7 ):
-#
-#		######### Prunning, Trimming, Filtering, cmsTopTagger
-#		if addGroomers:
-#			elemToKeep += [ 'keep *_'+jetalgo+'PFJetsCHSPrunedLinks_*_*', 
-#					'keep *_'+jetalgo+'PFJetsCHSTrimmedLinks_*_*', 
-#					'keep *_'+jetalgo+'PFJetsCHSFilteredLinks_*_*', 
-#					'keep *_cmsTopTagPFJetsCHSLinks'+jetALGO+'_*_*'
-#					]
-#			getattr( proc, 'patJets'+jetALGO+'PFCHS').userData.userFloats.src += [ jetalgo+'PFJetsCHSPrunedLinks',
-#											    jetalgo+'PFJetsCHSTrimmedLinks',
-#											    jetalgo+'PFJetsCHSFilteredLinks',
-#											    'cmsTopTagPFJetsCHSLinks'+jetALGO]
-#
-#			if 'CA' in jetALGO:
-#
-#				###### MassDrop
-#				elemToKeep += [ 'keep *_'+jetalgo+'PFJetsCHSMassDropFilteredLinks_*_*' ]
-#				getattr( proc, 'patJets'+jetALGO+'PFCHS').userData.userFloats.src += [ jetalgo+'PFJetsCHSMassDropFilteredLinks' ]
-#
-#				###### hepTopTagger
-#				if( jetSize > 1 ): 
-#					elemToKeep += [ 'keep *_hepTopTagPFJetsCHSLinks'+jetALGO+'_*_*' ]
-#					getattr( proc, 'patJets'+jetALGO+'PFCHS').userData.userFloats.src += [ 'hepTopTagPFJetsCHSLinks'+jetALGO ]
-#
-#		####### Nsubjettiness
-#		if addNsub:
-#			from RecoJets.JetProducers.nJettinessAdder_cfi import Njettiness
-#
-#			if addNsubUpTo5: setattr( proc, 'Njettiness'+jetALGO, Njettiness.clone( src = cms.InputTag( jetalgo+'PFJetsCHS'), cone = cms.double( jetSize ), Njets = cms.vuint32(1,2,3,4,5) ) )
-#			else: setattr( proc, 'Njettiness'+jetALGO, Njettiness.clone( src = cms.InputTag( jetalgo+'PFJetsCHS'), cone = cms.double( jetSize ) ) )
-#			elemToKeep += [ 'keep *_Njettiness'+jetALGO+'_*_*' ]
-#			getattr( proc, 'patJets'+jetALGO+'PFCHS').userData.userFloats.src += ['Njettiness'+jetALGO+':tau1','Njettiness'+jetALGO+':tau2','Njettiness'+jetALGO+':tau3']  
-#			if addNsubUpTo5: getattr( proc, 'patJets'+jetALGO+'PFCHS').userData.userFloats.src += [ 'Njettiness'+jetALGO+':tau4','Njettiness'+jetALGO+':tau5' ]
-#
-#		###### QJetsAdder
-#		if addQJets:
-#			### there must be a better way to do this random number introduction
-#			setattr( proc, 'RandomNumberGeneratorService', cms.Service("RandomNumberGeneratorService", 
-#								QJetsAdderCA8 = cms.PSet(initialSeed = cms.untracked.uint32(7)),
-#								QJetsAdderAK8 = cms.PSet(initialSeed = cms.untracked.uint32(31)),
-#								QJetsAdderCA15 = cms.PSet(initialSeed = cms.untracked.uint32(76)), ) )
-#
-#			from RecoJets.JetProducers.qjetsadder_cfi import QJetsAdder
-#			setattr( proc, 'QJetsAdder'+jetALGO, QJetsAdder.clone( src = cms.InputTag(jetalgo+'PFJetsCHS'), jetRad = cms.double( jetSize ), jetAlgo = cms.string( jetALGO[0:2] )))
-#			elemToKeep += [ 'keep *_QJetsAdder'+jetALGO+'_*_*' ]
-#			getattr( proc, 'patJets'+jetALGO+'PFCHS').userData.userFloats.src += ['QJetsAdder'+jetALGO+':QjetsVolatility']  
-#
-#
-#	### "return"
-#	if hasattr(proc, 'out'): getattr(proc, 'out').outputCommands += elemToKeep
-#	else: setattr( proc, 'out', cms.OutputModule('PoolOutputModule', 
-#							fileName = cms.untracked.string('newJettoolbox.root'), 
-#							outputCommands = cms.untracked.vstring( elemToKeep ) ) )
-#
